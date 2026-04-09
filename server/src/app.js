1c48ed3f6c5a0cf3659262f642ea3537c86c40cb
tykipanda <alejandro10londono@gmail.com>
2026-04-08 21:17:32 -0500

    declaracion const en server/src/app.js

---
+++ server/src/app.js
@@ -0,0 +1,9 @@
+require ('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cardRoutes = require('./routes/cards');
+const scryfallRoutes = require('./routes/scryfall');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
