d6da1826cc84c9c543d70752b46bee4679a140b7
tykipanda <alejandro10londono@gmail.com>
2026-04-10 22:03:46 -0500

    se agrego cardController en server/src/controllers/cardController.js

--- server/src/controllers/cardController.js
+++ server/src/controllers/cardController.js
@@ -1,2 +1,16 @@
 const deckService = require('../services/deckService');
 
+const cardController = {
+    async newGame(req, res, next) {
+        try {
+            const { deckCount = 1 } = req.body;
+            const deck = await deckService.createDeck(deckCount);
+            res.json({
+                success: true,
+                deckId: deck.deck_id,
+                remainiing: deck.remaining,
+            });
+        } catch (error) {next(error); }
+    },
+
+    
