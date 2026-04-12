1a2d70125906d6ac39aad795dd9f414bdb9861a5
tykipanda <alejandro10londono@gmail.com>
2026-04-11 21:33:20 -0500

    se agrego instancia de axios en server/src/services/scryfallService.js

--- server/src/services/scryfallService.js
+++ server/src/services/scryfallService.js
@@ -2,4 +2,12 @@ const axios = require('axios');
 
 const BASE_URL = 'https://api.scryfall.com';
 
-// Instancia de axios  con headers requeridos por scryfall
+// Instancia de axios  con headers requeridos por Scryfall
+const scryfallCLient = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'User-Agent': 'CardGameApp/1.0',
+        'Accept': 'application/json'
+    }
+});
+
