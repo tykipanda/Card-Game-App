dcf6486dceb6936c94c6453a05ab551f7cb89e8c
tykipanda <alejandro10londono@gmail.com>
2026-04-13 21:37:37 -0500

    se agregaron router en server/src/routes/scryfall.js

--- server/src/routes/scryfall.js
+++ server/src/routes/scryfall.js
@@ -2,3 +2,10 @@ const express = require ('express');
 const router = express.Router();
 const ctrl = require('../controllers/scryfallController');
 
+router.get('/search', ctrl.search);
+router.get('/named', ctrl.getByName);
+router.get('/random', ctrl.getRamdom);
+router.get('/card/:id', ctrl.getById);
+router.get('/autocomplete', ctrl.autocomplete);
+router.get('/sets', ctrl.getSets);
+
