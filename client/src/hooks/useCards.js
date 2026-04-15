7b70a9252cd9264e26545821887dd306a05313dc
tykipanda <alejandro10londono@gmail.com>
2026-04-14 22:08:32 -0500

    se agrego function useCards en client/src/hooks/useCards.js

--- client/src/hooks/useCards.js
+++ client/src/hooks/useCards.js
@@ -1,3 +1,11 @@
 import { useState, useCallback } from "react";
 import { cardAPI } from "../services/api";
 
+export function useCards() {
+    const [deckId, setDeckId] = useState (null);
+    const [hand, setHand] = useState ([]);
+    const [remaining, setRemaining] = useState (0);
+    const [loading, setLoading] = useState (false);
+    const [error, setError] = useState (null);
+
+}
