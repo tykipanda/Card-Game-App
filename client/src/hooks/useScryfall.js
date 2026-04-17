c4698c35690b5a304fa4180c01b18121b3becb98
tykipanda <alejandro10londono@gmail.com>
2026-04-16 22:15:02 -0500

    se agrego cards y randomCard en client/src/hooks/useScryfall.js

--- client/src/hooks/useScryfall.js
+++ client/src/hooks/useScryfall.js
@@ -1,3 +1,8 @@
 import { useState, useCallback } from "react";
 import { scryfallAPI } from "../services/scryfallApi";
 
+export function useScryfall() {
+    const [cards, setCards] = useState([]);
+    const [randomCard, setRandomCard] = useState([null]);
+    
+}
