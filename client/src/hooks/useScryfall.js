9f06c10ec12c02994ab45e7219c4b9c9c3eb68e0
tykipanda <alejandro10londono@gmail.com>
2026-04-16 22:18:39 -0500

    se agrego suggestions y loading en client/src/hooks/useScryfall.js

--- client/src/hooks/useScryfall.js
+++ client/src/hooks/useScryfall.js
@@ -4,5 +4,6 @@ import { scryfallAPI } from "../services/scryfallApi";
 export function useScryfall() {
     const [cards, setCards] = useState([]);
     const [randomCard, setRandomCard] = useState([null]);
-    
+    const [suggestions, setSuggestions] = useState([]);
+    const [loading, setLoading] = useState(false);
 }
