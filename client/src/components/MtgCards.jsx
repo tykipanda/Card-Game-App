806e0a78f238d39013b23eea47126e87639e1d35
tykipanda <alejandro10londono@gmail.com>
2026-04-19 22:23:34 -0500

    se agrego return style en client/src/components/MtgCard.jsx

--- client/src/components/MtgCards.jsx
+++ client/src/components/MtgCards.jsx
@@ -2,5 +2,18 @@ import React from "react";
 
 export default function MtgCard({ card, compact = false }) {
     if (!card) return null;
-    
+
+    if (compact) {
+        return (
+            <div style={{
+                width: '200px', borderRadius: '12px',
+                overflow: 'hidden', cursor: "pointer",
+                transition: 'transform 0.2s ease',
+                boxShadow: '0 4px 12px rgba(0,0,0,0.3)'
+            }}>
+                
+            </div>
+        )
+    }
+
 }
