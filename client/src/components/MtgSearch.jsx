9c77a20a9bae97b0b6b809126adf763b35179b61
tykipanda <alejandro10londono@gmail.com>
2026-04-20 21:30:52 -0500

    se agrego function en client/src/components/MtgSearch.jsx

--- client/src/components/MtgSearch.jsx
+++ client/src/components/MtgSearch.jsx
@@ -1,3 +1,13 @@
 import React, { useState, useEffect, useRef} from "react";
 import { useScryfall } from '../hooks/useScryfall';
 import MtgCard from "./MtgCards";
+
+export default function MtgSearch() {
+    const [query, setQuery] = useState('');
+    const{
+        card, loading, error, totalCards, hasMore,
+        searchCards, loadMore, fetchAutocomplete, 
+        suggestions, clearResults
+    } = useScryfall();
+    
+;}
