9bb5e6630b3f7d0ce7394beb6eab4feaaaba3087
tykipanda <alejandro10londono@gmail.com>
2026-04-22 11:48:45 -0500

    se agrego return h2 en client/src/components/MtgRandomCard.jsx

--- client/src/components/MtgRandomCard.jsx
+++ client/src/components/MtgRandomCard.jsx
@@ -4,4 +4,14 @@ import MtgCard from "./MtgCards";
 
 export default function MtgRandomCard() {
     const { randomCard, loading, fetchRandom } = useScryfall();
+
+    return (
+        <div style={{ textAlign: 'center', padding: '20px'}}>
+            <h2 style={{ color: '#fff' }}>Carta Aleatoria</h2>
+            <div  style={{ display: 'flex', gap: '12px',
+                justifyContent: 'center', marginBottom: '24px'}}>
+                    
+                </div>
+        </div>
+    )
 }
