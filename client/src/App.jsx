9d8b25ad1999e6b10c9c248b20320e18733c52ee
tykipanda <alejandro10londono@gmail.com>
2026-04-22 15:47:19 -0500

    se agrego import en documento nuevo client/src/App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+import React, { useState} from "react";
+import GameBoard from './components/GameBoard';
+import MtgSearch from './components/MtgSearch';
+import MtgRandomCard from "./components/MtgRandomCard";
+
